8220c306d57417d3b3f72a22fa9360b1e6a38ba3
yijun <sandy3068@yahoo.com.tw>
2021-08-07 14:57:14 +0800

    Update 2021-08-07T06:57:04.710Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,39 +1,43 @@
-const menuItem = document.querySelectorAll('.js-menu a');
-const productsNav = document.querySelector('.js-productsNav');
-const productsNavItem = document.querySelectorAll('.js-productsNav a');
-const productsPages = document.querySelectorAll('.js-productsPages .js-page');
-const main = document.querySelector('main');
+"use strict";
 
-//在menu加上active
+var menuItem = document.querySelectorAll('.js-menu a');
+var productsNav = document.querySelector('.js-productsNav');
+var productsNavItem = document.querySelectorAll('.js-productsNav a');
+var productsPages = document.querySelectorAll('.js-productsPages .js-page');
+var main = document.querySelector('main'); //在menu加上active
+
 function getActiveData() {
-  menuItem.forEach ((item) => {
+  menuItem.forEach(function (item) {
     if (item.getAttribute('data-current') === item.getAttribute('data-name')) {
       item.setAttribute('class', 'active');
     }
   });
 }
-getActiveData();
 
-//產品業分頁切換
+getActiveData(); //產品業分頁切換
+
 if (main.getAttribute('class') === 'products') {
-  productsNav.addEventListener('click', (e) => {
+  productsNav.addEventListener('click', function (e) {
     e.preventDefault();
+
     if (e.target.getAttribute('data-btnName') === null) {
       return;
     }
-    productsNavItem.forEach((item) => {
+
+    productsNavItem.forEach(function (item) {
       if (item.getAttribute('data-btnName') === e.target.getAttribute('data-btnName')) {
         item.setAttribute('class', 'active');
       } else {
         item.setAttribute('class', '');
       }
-    })
-    productsPages.forEach((item) => {
+    });
+    productsPages.forEach(function (item) {
       if (e.target.getAttribute('data-btnName') === item.getAttribute('data-pageName')) {
         item.setAttribute('class', 'js-page show');
       } else {
-        item.setAttribute('class', 'js-page hide')
+        item.setAttribute('class', 'js-page hide');
       }
-    })
-  })
+    });
+  });
 }
+//# sourceMappingURL=all.js.map
